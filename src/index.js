c1be30ec9972c61615d2ef09852ce85eaf8ca62f
Jagadeesh-Vadlamuri <94168608+Jagadeesh-Vadlamuri@users.noreply.github.com>
2022-02-21 12:40:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ToDoApp from './ToDoList';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
